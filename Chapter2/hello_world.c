69837758d8186df162d7c558cf86c2c3403ff868
fujx <73429424+fujx-star@users.noreply.github.com>
2023-12-27 01:03:37 +0800

    Create hello_world.c

---
+++ Chapter2/hello_world.c
@@ -0,0 +1,16 @@
+// hello_world.c
+
+// 该源文件的执行需要包含标准输入输出文件 stdio.h
+#include <stdio.h>
+
+// 声明一个名为"main"的函数，函数返回值类型为int
+// main函数是一个C程序执行的入口，每个C程序中必须有一个main函数，这个入口函数名称固定
+int main() {
+    
+    // 调用标准输出函数printf在输出设备上打印"hello world"
+    printf("hello world");
+    
+    // 函数返回0，表示函数的执行结束
+    return 0;
+    
+}

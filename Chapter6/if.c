7898cc717cd266a7beda703886ae56344d419890
fujx <73429424+fujx-star@users.noreply.github.com>
2024-01-12 21:01:22 +0800

    Create if.c

---
+++ Chapter6/if.c
@@ -0,0 +1,19 @@
+// if.c
+// if的使用
+#include <stdio.h>
+
+int main() {
+    
+    int a = 4;
+    
+    if (a <= 5) {
+        if (a >= 3) {
+            printf("The value of a is between 3 and 5.");
+        }
+    }
+    
+    return 0;
+    
+}
+// 输出：
+// The value of a is between 3 and 5.

50d2c385eecb54fd117dda5d48e1f37af9241926
fujx <73429424+fujx-star@users.noreply.github.com>
2024-01-27 11:31:21 +0800

    Add files via upload

---
+++ Chapter7/strcpy.c
@@ -0,0 +1,21 @@
+// strcpy.c
+// 字符串复制函数strcpy()的使用
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	char a[20], b[20];
+
+	strcpy(a, "hello world");
+	strcpy(b, a);
+
+	puts(a);
+	puts(b);
+
+	return 0;
+
+}
+// 输出
+// hello world
+// hello world
